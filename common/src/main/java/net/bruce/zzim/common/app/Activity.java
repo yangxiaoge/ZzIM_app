cda8af6c2815fe6e622e6bf271ffd18a198f6da5
yang.jianan <yangjianan@begoit.com>
2017-07-20 19:54:42 +0800

    add common库

--- common/src/main/java/net/bruce/zzim/common/app/Activity.java
+++ common/src/main/java/net/bruce/zzim/common/app/Activity.java
@@ -12,10 +12,34 @@ public abstract class Activity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //在界面初始化之前调用的初始化窗口
+        initWindows();
 
-        getContentLayoutId();
-        initWidget();
-        initData();
+        if (initArgs(getIntent().getExtras())) {
+            getContentLayoutId();
+            initWidget();
+            initData();
+        } else {
+            finish();
+        }
+
+    }
+
+    /**
+     * 初始化窗口
+     */
+    protected void initWindows() {
+
+    }
+
+    /**
+     * 初始化相关参数
+     *
+     * @param bundle 参数bundle
+     * @return 如果参数正确返回true，错误返回false
+     */
+    protected boolean initArgs(Bundle bundle) {
+        return true;
     }
 
     /**
@@ -37,5 +61,23 @@ public abstract class Activity extends AppCompatActivity {
      */
     protected void initData() {
 
+    }
+
+    /**
+     * 点击导航返回上一页
+     * @return
+     */
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+
+
+        super.onBackPressed();
+        finish();
     }
 }
--- common/src/main/java/net/bruce/zzim/common/app/Fragment.java
+++ common/src/main/java/net/bruce/zzim/common/app/Fragment.java
@@ -1,8 +1,77 @@
 package net.bruce.zzim.common.app;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by yang.jianan on 2017/07/20 17:19.
  */
 
-public class Fragment {
+public abstract class Fragment extends android.support.v4.app.Fragment {
+    protected View root;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        initArgs(getArguments());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (root == null) {
+            int layoutId = getContentLayoutId();
+            View viewRoot = inflater.inflate(layoutId, container, false);
+            initWidget(viewRoot);
+            root = viewRoot;
+        } else {
+            if (root.getParent() != null) {
+                ((ViewGroup) root.getParent()).removeView(root);
+            }
+        }
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //当View创建完成后初始化数据
+        initData();
+    }
+
+    /**
+     * 初始化相关参数
+     *
+     * @param bundle 参数bundle
+     * @return 如果参数正确返回true，错误返回false
+     */
+    protected void initArgs(Bundle bundle) {
+
+    }
+
+    /**
+     * 获取当前页面资源文件Id
+     *
+     * @return 资源文件Id
+     */
+    protected abstract int getContentLayoutId();
+
+    /**
+     * 初始化控件
+     */
+    protected void initWidget(View viewRoot) {
+
+    }
+
+    /**
+     * 初始化数据
+     */
+    protected void initData() {
+
+    }
 }
