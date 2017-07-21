114d5f39668167bb5a096f92a0ef070c24f36025
yang.jianan <364804051@qq.com>
2017-07-21 17:44:32 +0800

    add res

--- app/src/main/java/net/bruce/zzim/push/MainActivity.java
+++ app/src/main/java/net/bruce/zzim/push/MainActivity.java
@@ -1,20 +1,8 @@
 package net.bruce.zzim.push;
 
-import android.widget.EditText;
-import android.widget.TextView;
-
 import net.bruce.zzim.common.app.Activity;
 
-import butterknife.BindView;
-import butterknife.OnClick;
-
-public class MainActivity extends Activity implements IView {
-    @BindView(R.id.text_test)
-    TextView mTestText;
-    @BindView(R.id.edit_test)
-    EditText mEditText;
-
-    private IPresenter presenter;
+public class MainActivity extends Activity {
 
     @Override
     protected int getContentLayoutId() {
@@ -24,21 +12,6 @@ public class MainActivity extends Activity implements IView {
     @Override
     protected void initData() {
         super.initData();
-        presenter = new Presenter(this);
     }
 
-    @OnClick(R.id.submit_test)
-    void onSubmit() {
-        presenter.search();
-    }
-
-    @Override
-    public String getInputString() {
-        return mEditText.getText().toString();
-    }
-
-    @Override
-    public void setResultString(String text) {
-        mTestText.setText(text);
-    }
 }
